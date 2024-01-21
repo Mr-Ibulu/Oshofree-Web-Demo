402327a3825fa1e43aa1521b3d05838a2c1c49dc
Mr-Ibulu <ibululawrence@gmail.com>
2024-01-21 15:01:18 +0100

    revert changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 import withPlaiceholder from "@plaiceholder/next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
+  output: "export",
   images: {
     unoptimized: true,
     remotePatterns: [
